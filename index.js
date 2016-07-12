fe1062fca04fd96c9adf78916b56b0b21ccc210c
Victor Perin <me@victorperin.com>
2016-07-12 01:12:18 -0300

    fixing style

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 //dependencies
-var fs      = require('fs'),
-    request = require('request'),
-    cheerio = require('cheerio');
-    shelljs = require('shelljs/global');
-    moment  = require('moment');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var shelljs = require('shelljs/global');
+var moment  = require('moment');
 
-var url = "http://cmmcd.com/PokemonGo/";
+var url = 'http://cmmcd.com/PokemonGo/';
 var lastValue = null;
 
-console.log("Pokemon GO - Server Status");
+console.log('Pokemon GO - Server Status');
 func();
 setInterval(func, 20 * 1000);
 
-function func(){
-    request(url, function(err, res, html){
-        if(err) throw err;
-        var $ = cheerio.load(html);
-        var status = $($('.jumbotron h2 font')[0]).html();
+function func() {
+  request(url, function (err, res, html) {
+    if (err) throw err;
+    var $ = cheerio.load(html);
+    var status = $($('.jumbotron h2 font')[0]).html();
 
-        if(status !== lastValue){
-            exec('say ' + status);
+    if (status !== lastValue) {
+      exec('say ' + status);
 
-            console.log(moment().format('h:mm:ss a') + " - " + status);
-            lastValue = status;
-        }
-    });
+      console.log(moment().format('h:mm:ss a') + ' - ' + status);
+      lastValue = status;
+    }
+  });
 }
